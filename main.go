81899150e96507a603d81959924e6f1dd0e67716
PCPedroso <pclpedroso@gmail.com>
2024-06-08 10:50:39 -0300

    Iniciando projeto

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func main() {
+	req, err := http.Get("https://www.google.com/")
+	if err != nil {
+		panic(err)
+	}
+	defer req.Body.Close()
+
+	res, err := io.ReadAll(req.Body)
+	fmt.Println(string(res))
+}
